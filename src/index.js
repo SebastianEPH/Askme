500c73f3fce437d39f08b500e4bc6afaac5a9780
SebastianEPH <sebastianeph99@gmail.com>
2020-11-13 16:32:39 -0500

    bases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const app = express()
+const path = require('path')
+const morgan = require('morgan')  // Libs for middlewares
+const app = express();
+
+//Settings
+app.set('port',process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// middlewares
+app.use(morgan('dev'));
+// router
 
 
-app.listen(3000,()=>{
+app.listen(app.get('port'),()=>{
     console.log("Server on port 3000")
 })
