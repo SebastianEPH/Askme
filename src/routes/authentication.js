d0a1a9b50f6b05fa3135612614e5980aaf18ad02
sebastianeph <sebastianeph99@gmail.com>
2020-12-12 01:57:19 -0500

    singup + frontend

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/singup', (req, res)=>{
+    res.render('auth/singup')
+})
+router.post('/singup',(req, res)=>{
 
-
+})
 module.exports = router;
